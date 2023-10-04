ba8f63c78f8280dd0b9de360a9b69369b87056bd
pauldelucia <pauldelucia2@gmail.com>
2023-10-04 20:11:39 +0700

    added storage-cost-calculator.py to the project

---
+++ storage-cost-calculator.py
@@ -0,0 +1,22 @@
+# Enter the years here
+years = 2
+
+# Constants
+C_0=450
+initial_decline=0.15
+softening_factor=0.965
+
+def cumulative_storage_cost(t):
+    '''Calculates the cumulative storage cost on Dash Platform for t years of storage'''
+    total_cost = 0
+    current_cost = C_0
+    current_decline = initial_decline
+    
+    for _ in range(t):
+        total_cost += current_cost
+        current_cost *= (1 - current_decline)
+        current_decline *= softening_factor
+        
+    return total_cost
+
+print(f"{cumulative_storage_cost(years):.0f} Credits")
